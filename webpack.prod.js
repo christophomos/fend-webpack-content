5a204cdf4d152d4bd3646992dfad4c9b1d942440
Chris Paliqaw <christophomos@gmail.com>
2022-12-08 16:30:09 +0100

    feature: minify css in production feature: add service worker for offline access

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,11 @@
 const path = require("path");
 const webpack = require("webpack");
 const htmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
     mode: 'production',
     // use instead of clean-webpack-plugin
@@ -22,7 +27,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]   
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],  
             }
         ]
     },
@@ -30,6 +35,11 @@ module.exports = {
         new htmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html"
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new WorkboxPlugin.GenerateSW()
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
 };
